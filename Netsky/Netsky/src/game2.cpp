40de9aacd833e8783dac45fae713c44497cbe29c
IRkeserdzhieva23 <IRkeserdzhieva23@codingburgas.bg>
2024-11-24 15:16:35 +0200

    Add finished game 2

--- Netsky/Netsky/src/game2.cpp
+++ Netsky/Netsky/src/game2.cpp
@@ -1,5 +1,4 @@
 #include <raylib.h>
-
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -34,28 +33,26 @@ GameState game2()
 
     //--------------------------------------------------------------------------------------
     std::string wordList[] = { "product",
-       "ordinal", "divisor", "percent" , "algebra" }; // the words we might get
-    std::string ChosenWord = wordList[rand() % 5]; // choosing a word from the wordlist 
-
+       "ordinal", "divisor", "percent" , "algebra" }; // initialize the words we might get
+    std::string chosenWord = wordList[rand() % 5]; // choosing a word from the wordlist 
     char underscores[MAX_ITEMSSSIZE];
     char name[MAX_ITEMSSSIZE] = { '\0' };
-    // find how many underscores we have to draw
-    for (int i = 0; i < ChosenWord.length(); i++)
+    
+    for (int i = 0; i < chosenWord.length(); i++)// find how many underscores we have to draw
     {
         underscores[i] = '_';
     }
-    underscores[ChosenWord.length()] = '\0';  // Null-terminate the string
+    underscores[chosenWord.length()] = '\0';  // Null-terminate the string
 
-    Rectangle textBox = { screenWidth / 2.0f - 140 , 70, 250, 70 };
-    int LetterCount = 0;
-    int MaxInput = ChosenWord.size();
+  
+    int letterCount = 0;
+    int maxInput = chosenWord.size();
     int lives = 5;
     bool found = false;
     int foundcount = 0;
-    Rectangle textBox2 = { screenWidth / 2.0f + 200, 150, 280, 60 };
-    int underscoreX = textBox.x + 3;
-    int underscoreY = textBox.y + 20;
- 
+   
+
+    // initializing textures and fonts
     Texture2D hangman0 = LoadTexture("graphics/Hangman0.png");
     Texture2D hangman5 = LoadTexture("graphics/Hangman5.png");
     Texture2D hangman4 = LoadTexture("graphics/Hangman4.png");
@@ -64,9 +61,9 @@ GameState game2()
     Texture2D hangman1 = LoadTexture("graphics/Hangman1.png");
     Texture2D background = LoadTexture("graphics/squaredpapersheet.png");
     Font fontBt = LoadFontEx("fonts/Kalam-Bold.ttf", 75, 0, 250);
-    Button restartButton{ "graphics/Restart_button.png", {140, 660}, 0.6f };
-    Button homeButton{ "graphics/Home_button.png", {460, 660}, 0.6f };
-    Button quitButton{ "graphics/Quit_button2.png", {780, 660}, 0.6f };
+    Button restartButton{ "graphics/Restart_button.png", {100, 660}, 0.4f };
+    Button homeButton{ "graphics/Home_button.png", {300, 660}, 0.4f };
+    Button quitButton{ "graphics/Quit_button2.png", {500, 660}, 0.4f };
    
 
 
@@ -77,7 +74,6 @@ GameState game2()
     while (!WindowShouldClose())    
     {
         
-
         int letter = GetCharPressed();
         Vector2 mousePosition = GetMousePosition();
         bool mousePressed = IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
@@ -89,32 +85,32 @@ GameState game2()
 
             {
 
-                name[LetterCount] = (char)letter; 
-                    name[LetterCount + 1] = '\0'; // Add null terminator at the end of the string
+                name[letterCount] = (char)letter; 
+                    name[letterCount + 1] = '\0'; // Add null terminator at the end of the string
 
-                LetterCount++;
+                letterCount++;
 
             }
             letter = GetCharPressed();  // Check next character in the queue
         }
 
-        if (IsKeyPressed(KEY_BACKSPACE))
+        if (IsKeyPressed(KEY_BACKSPACE)) // delete the last entered letter 
         {
-            LetterCount--;
-            if (LetterCount < 0)
+            letterCount--;
+            if (letterCount < 0) //if there are no letters entered, return letterCount to zero
             {
-                LetterCount = 0;
-                name[LetterCount] = '\0';
+                letterCount = 0;
+                name[letterCount] = '\0';
             }
 
         }
         // check if the word has the letter the user entered
         if (IsKeyPressed(KEY_ENTER)) {
-            char letterToCheck = name[LetterCount - 1];
+            char letterToCheck = name[letterCount - 1];
             bool foundLetter = false;
 
-            for (int i = 0; i < ChosenWord.size(); i++) {
-                if (ChosenWord[i] == letterToCheck) {
+            for (int i = 0; i < chosenWord.size(); i++) {
+                if (chosenWord[i] == letterToCheck) {
                     foundLetter = true;
                     ReplaceSpecificUnderscore(underscores, i + 1, letterToCheck);  // +1 because underscores are 1-indexed in logic
                     //the letter has been found and replaced
@@ -137,76 +133,79 @@ GameState game2()
         DrawTexture(background, 0, 0, WHITE);
 
 
-        DrawTextEx(fontBt, "GUESS THE WORD:", { 70, 70 }, (float)fontBt.baseSize, 30, BLACK);
-
-        // draw the underscores
-        DrawTextEx(fontBt, underscores, { textBox.x- 400, textBox.y + 105 }, (float)fontBt.baseSize, 60, BLACK);
-        DrawRectangleLines(300, 300, (int)textBox2.width, (int)textBox2.height, DARKBLUE);
-
-
-
-        DrawTextEx(fontBt, TextFormat( "Letters left: %i/%i", foundcount, MaxInput), { 500, 550 }, (float)fontBt.baseSize, 1, DARKBLUE);
-
-
+        DrawTextEx(fontBt, "GUESS THE WORD:", { 70, 56 }, (float)fontBt.baseSize, 30, BLACK);
+        DrawTextEx(fontBt, "(try guessing math terms)", { 30, 120 }, (float)fontBt.baseSize - 36, 1, DARKBLUE);
+        DrawTextEx(fontBt, TextFormat( "Letters left: %i/%i", foundcount, maxInput), { 1170, 700 }, (float)fontBt.baseSize - 24, 1, DARKBLUE);
         DrawTextEx(fontBt, "Guess a letter ", { 70, 260 }, (float)fontBt.baseSize, 30, BLACK);
         DrawTextEx(fontBt, "and press ENTER : ", { 70, 340 }, (float)fontBt.baseSize, 15, BLACK);
-        DrawTextEx(fontBt, name, { 200, 400 }, (float)fontBt.baseSize, 60, BLACK);
 
+        // draw the underscores
+        DrawTextEx(fontBt, underscores, { 180, 70 + 105 }, (float)fontBt.baseSize, 60, BLACK);
+
+        // draw user's input
+        DrawTextEx(fontBt, name, { 100, 400 }, (float)fontBt.baseSize + 30, 40, DARKBLUE);
+
+        // draw the hangman when you have 5 lives
         if (lives == 5)
         {
-            DrawTextEx(fontBt, "Lives left: 5/5", { 500, 500 }, (float)fontBt.baseSize, 1, DARKBLUE);
+            DrawTextEx(fontBt, "Lives left: 5/5", { 1200, 80 }, (float)fontBt.baseSize - 24, 1,RED);
             DrawTexture(hangman5, screenWidth / 2 - hangman5.width / 2, screenHeight / 2 - hangman5.height / 2, WHITE);
-            if ((lives != 0) && (LetterCount == MaxInput))
+            if ((lives != 0) && (letterCount == maxInput))
             {
-                DrawTextEx(fontBt, "GAME WON", { 350, 400 }, (float)fontBt.baseSize, 60, DARKGREEN);
+                DrawTextEx(fontBt, "GAME WON", { 70, 500 }, (float)fontBt.baseSize, 17, DARKGREEN);  // draw Game Won if the user has guessed
             }
         }
-
+        // draw the hangman when you have 4 lives
         if (lives == 4)
         {
-            DrawTextEx(fontBt, "Lives left: 4/5", { 500, 550 }, (float)fontBt.baseSize, 50, DARKBLUE);
+            DrawTextEx(fontBt, "Lives left: 4/5", { 1200, 80 }, (float)fontBt.baseSize - 24, 1, RED);
             DrawTexture(hangman4, screenWidth / 2 - hangman4.width / 2, screenHeight / 2 - hangman4.height / 2, WHITE);
-            if ((lives != 0) && (foundcount == MaxInput))
+            if ((lives != 0) && (foundcount == maxInput))
             {
-                DrawTextEx(fontBt, "GAME WON", { 350, 400 }, (float)fontBt.baseSize, 60, DARKGREEN);
+                DrawTextEx(fontBt, "GAME WON", { 70, 500 }, (float)fontBt.baseSize, 60, DARKGREEN); // draw Game Won if the user has guessed
             }
         }
-
+        // draw the hangman when you have 3 lives
         if (lives == 3)
         {
-            DrawTextEx(fontBt, "Lives left: 3/5", { 500, 550 }, (float)fontBt.baseSize, 50, DARKBLUE);
+            DrawTextEx(fontBt, "Lives left: 3/5", { 1200, 80 }, (float)fontBt.baseSize - 24, 1, RED);
             DrawTexture(hangman3, screenWidth / 2 - hangman3.width / 2, screenHeight / 2 - hangman3.height / 2, WHITE);
-            if ((lives != 0) && (foundcount == MaxInput))
+            if ((lives != 0) && (foundcount == maxInput))
             {
-                DrawTextEx(fontBt, "GAME WON", { 350, 400 }, (float)fontBt.baseSize, 60, DARKGREEN);
+                DrawTextEx(fontBt, "GAME WON", { 70, 500 }, (float)fontBt.baseSize, 60, DARKGREEN);  // draw Game Won if the user has guessed
+                
             }
         }
+        // draw the hangman when you have 2 lives
         if (lives == 2)
         {
-            DrawTextEx(fontBt, "Lives left: 2/5", { 500, 550 }, (float)fontBt.baseSize, 50, DARKBLUE);
+            DrawTextEx(fontBt, "Lives left: 2/5", { 1200, 80 }, (float)fontBt.baseSize - 24, 1, RED);
             DrawTexture(hangman2, screenWidth / 2 - hangman2.width / 2, screenHeight / 2 - hangman2.height / 2, WHITE);
-            if ((lives != 0) && (foundcount == MaxInput))
+            if ((lives != 0) && (foundcount == maxInput))
             {
-                DrawTextEx(fontBt, "GAME WON", { 350, 400 }, (float)fontBt.baseSize, 60, DARKGREEN);
+                DrawTextEx(fontBt, "GAME WON", { 70, 500 }, (float)fontBt.baseSize, 60, DARKGREEN); // draw Game Won if the user has guessed
             }
         }
+        // draw the hangman when you have 1 live
         if (lives == 1)
         {
-            DrawTextEx(fontBt, "Lives left: 1/5", { 500, 550 }, (float)fontBt.baseSize, 50, DARKBLUE);
+            DrawTextEx(fontBt, "Lives left: 1/5", { 1200, 80 }, (float)fontBt.baseSize - 24, 1, RED);
             DrawTexture(hangman1, screenWidth / 2 - hangman1.width / 2, screenHeight / 2 - hangman1.height / 2, WHITE);
-            if ((lives != 0) && (foundcount == MaxInput))
+            if ((lives != 0) && (foundcount == maxInput)) // draw Game Won if the user has guessed
             {
-                DrawTextEx(fontBt, "GAME WON", { 350, 400 }, (float)fontBt.baseSize, 60, DARKGREEN);
+                DrawTextEx(fontBt, "GAME WON", { 70, 500 }, (float)fontBt.baseSize, 60, DARKGREEN);
             }
 
+            // draw the hangman when you have 0 lives
         }if (lives == 0)
         {
-            DrawTextEx(fontBt, "Lives left: 0/5", { 500, 400 }, (float)fontBt.baseSize, 50, RED);
-            DrawTextEx(fontBt, "GAME OVER", { 350, 400 }, (float)fontBt.baseSize, 60, RED);
+            DrawTextEx(fontBt, "Lives left: 0/5", { 1200, 80 }, (float)fontBt.baseSize - 24, 1, RED);
+            DrawTextEx(fontBt, "GAME OVER", { 70, 500 }, (float)fontBt.baseSize, 60, RED);
             DrawTexture(hangman0, screenWidth / 2 - hangman0.width / 2, screenHeight / 2 - hangman0.height / 2, WHITE);
 
 
         }
+        //draw restert, home and quit buttons
         restartButton.Draw();
         homeButton.Draw();
         quitButton.Draw();
